ac986fbdf5335de3f2566979c10bd87875090bf1
Bandelasrivigna <bandelasrivignareddy@gmail.com>
2026-03-15 17:39:48 -0500

    Phase 1 GroupFlight project

---
+++ Areas/Admin/Controllers/UsersController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Group2Flight.Areas.Admin.Controllers
+{
+	[Area("Admin")]
+	public class UsersController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult Manage()
+		{
+			return Content("Admin Manage Users Page");
+		}
+
+		public IActionResult Rights()
+		{
+			return Content("Admin Rights and Obligations Page");
+		}
+	}
+}
---
+++ Areas/Airlines/Controllers/FlightsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Group2Flight.Areas.Airlines.Controllers
+{
+	[Area("Airlines")]
+	public class FlightsController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult Manage()
+		{
+			return Content("Airline Manage Flights Page");
+		}
+
+		public IActionResult Regulation()
+		{
+			return Content("Airline Regulation Page");
+		}
+	}
+}
